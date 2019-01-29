daf1ca0e657989ed61e1691cdce9966a0cc4dbf3
Eric LI <erli@student.42.fr>
2019-01-29 15:02:17 +0100

    Partons de bon pied, MAJ du header

--- includes/lem_in_algo.h
+++ includes/lem_in_algo.h
@@ -6,7 +6,7 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 17:30:26 by erli              #+#    #+#             */
-/*   Updated: 2019/01/28 18:11:43 by erli             ###   ########.fr       */
+/*   Updated: 2019/01/29 15:01:20 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,23 @@ typedef struct	s_room
 	int			id;
 	int			x;
 	int			y;
-	int			*neighboor;
-	char		properties;
-	int			occupant;
 }				t_room;
+
+typedef struct	s_lem_in_data
+{
+	int			n_ant;
+	int			start;
+	int			end;
+	s_room		*anthill;
+	char		**matrix;
+}
+
 
 typedef	struct	s_path
 {
 	int		path_id;
 	int		*path;
-	int		mother_id;
+	int		steps;
 }				t_path;
 
 #endif
--- srcs/lem_in_algo.c
+++ srcs/lem_in_algo.c
@@ -6,13 +6,13 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 17:49:28 by erli              #+#    #+#             */
-/*   Updated: 2019/01/28 17:59:33 by erli             ###   ########.fr       */
+/*   Updated: 2019/01/28 18:20:47 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in_algo.h"
 
-void			lem_in_algo(t_room *anthill, int count)
+void			lem_in_algo(t_room *anthill, int count, int options)
 {
 
 
