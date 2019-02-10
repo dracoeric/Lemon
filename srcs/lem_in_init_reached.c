f8548b9d9cca4d2c8c9994dbe114c7c85abe4def
Eric LI <erli@student.42.fr>
2019-02-10 17:07:16 +0100

    Legere modifcation de init reached pour data == 0

--- srcs/lem_in_init_reached.c
+++ srcs/lem_in_init_reached.c
@@ -6,7 +6,7 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 16:39:11 by erli              #+#    #+#             */
-/*   Updated: 2019/02/09 16:52:53 by erli             ###   ########.fr       */
+/*   Updated: 2019/02/10 17:05:58 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int			lem_in_init_reached(t_lem_in_data *data)
 	int i;
 	int	*reached;
 
+	if (data == 0)
+		return (-1);
 	if (!(reached = (int *)malloc(sizeof(int) * data->n_room)))
 		return (ft_msg_int(2, "Failed malloc for reached\n", -2));
 	i = 0;
